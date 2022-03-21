4a14454795d8c4236774afe16f2f84d7f47fd8c0
serge shustoff <serge.shustoff@gmail.com>
2022-03-21 22:28:50 +0100

    fix android publishing

--- ktsqlite-android/build.gradle.kts
+++ ktsqlite-android/build.gradle.kts
@@ -2,14 +2,30 @@ plugins {
     id("com.android.library")
     id("maven-publish")
 }
-group = "com.github.sergeshustoff"
-version = "1.0.0"
 
 android {
     compileSdk = 31
     defaultConfig {
         minSdk = 23
         targetSdk = 31
+    }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+}
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.github.sergeshustoff"
+            artifactId = "ktsqlite-android"
+            version = "1.0.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
     }
 }
 
